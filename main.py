09d202583dd4f368ab9aa769d913848b5f557e46
Oleh Melnyk <mr.oleg.melnyk@gmail.com>
2020-11-22 01:02:02 +0200

    Code refactor

--- main.py
+++ main.py
@@ -10,39 +10,38 @@ import tensorflow as tf
 # video_extractor.extract('data/lake_in_park.MOV')
 
 SIMILARITY_LOWER_BOUND = 0.6
+DATA_ROOT_FOLDER = 'data/'
 
 
 def main():
-    for model_type in [ModelType.efficientnet_b4, ModelType.resnet_v2_50, ModelType.image_net_inception_v3,
-                       ModelType.inception_resnet_v2, ModelType.inaturalist_inception_v3]:
+    for model_type in ModelType:
         feature_extractor_model = ModelProvider.get_model(model_type)
-
         testing(feature_extractor_model, model_type)
 
 
 def testing(feature_extractor_model, model_type):
-    calculate_similarity(feature_extractor_model, model_type, 'data/lion.jpg', 'data/lion2.jpg')
-    calculate_similarity(feature_extractor_model, model_type, 'data/lion.jpg', 'data/men.jpeg')
-    calculate_similarity(feature_extractor_model, model_type, 'data/dog.jpeg', 'data/men.jpeg')
-    calculate_similarity(feature_extractor_model, model_type, 'data/men2.jpeg', 'data/men.jpeg')
-    calculate_similarity(feature_extractor_model, model_type, 'data/women.jpeg', 'data/men.jpeg')
-    calculate_similarity(feature_extractor_model, model_type, 'data/women.jpeg', 'data/men2.jpeg')
-    calculate_similarity(feature_extractor_model, model_type, 'data/lions1.jpg', 'data/lions2.jpeg')
-    calculate_similarity(feature_extractor_model, model_type, 'data/lion.jpg', 'data/lions2.jpeg')
-    calculate_similarity(feature_extractor_model, model_type, 'data/lion.jpg', 'data/dog.jpeg')
-    calculate_similarity(feature_extractor_model, model_type, 'data/lions1.jpg', 'data/dog.jpeg')
+    calculate_similarity(feature_extractor_model, model_type, 'lion.jpg', 'lion2.jpg')
+    calculate_similarity(feature_extractor_model, model_type, 'lion.jpg', 'men.jpeg')
+    calculate_similarity(feature_extractor_model, model_type, 'dog.jpeg', 'men.jpeg')
+    calculate_similarity(feature_extractor_model, model_type, 'men2.jpeg', 'men.jpeg')
+    calculate_similarity(feature_extractor_model, model_type, 'women.jpeg', 'men.jpeg')
+    calculate_similarity(feature_extractor_model, model_type, 'women.jpeg', 'men2.jpeg')
+    calculate_similarity(feature_extractor_model, model_type, 'lions1.jpg', 'lions2.jpeg')
+    calculate_similarity(feature_extractor_model, model_type, 'lion.jpg', 'lions2.jpeg')
+    calculate_similarity(feature_extractor_model, model_type, 'lion.jpg', 'dog.jpeg')
+    calculate_similarity(feature_extractor_model, model_type, 'lions1.jpg', 'dog.jpeg')
 
 
 def calculate_similarity(feature_extractor_model: Model, model_type: ModelType, image_path1: str, image_path2: str):
-    image_array1 = ImageUtils.load_and_preprocess_image(image_path1, model_type.SHAPE_SIZE)
-    image_array2 = ImageUtils.load_and_preprocess_image(image_path2, model_type.SHAPE_SIZE)
+    image_array1 = ImageUtils.load_and_preprocess_image(DATA_ROOT_FOLDER + image_path1, model_type.SHAPE_SIZE)
+    image_array2 = ImageUtils.load_and_preprocess_image(DATA_ROOT_FOLDER + image_path2, model_type.SHAPE_SIZE)
     # ImageUtils.show_image(image_array)
     features1 = feature_extractor_model.predict(np.array([image_array1]))
     features2 = feature_extractor_model.predict(np.array([image_array2]))
     cosine_loss = tf.keras.losses.CosineSimilarity(axis=1)
     similarity_value = cosine_loss(y_true=tf.nn.l2_normalize(features1), y_pred=tf.nn.l2_normalize(features2)).numpy()
     if abs(similarity_value) >= SIMILARITY_LOWER_BOUND:
-        print('{} and {} similarity: {}'.format(image_path1, image_path2, similarity_value))
+        print('{} and {} similarity: {:.2f}%'.format(image_path1, image_path2, abs(similarity_value * 100)))
 
 
 if __name__ == "__main__":
