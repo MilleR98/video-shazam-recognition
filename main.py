45036ff9a5a821e932e6baa4f75f65b9812464db
OMelnykJapp <mr.oleg.melnyk@gmail.com>
2021-01-16 20:41:10 +0200

    Added stub index html page to root route. Updated run_server.sh to handle prod and dev env

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from cfg.globals import LOG_CONF_FILE, BASE_DIR
 from logger_wrapper import Log
@@ -11,6 +11,11 @@ app = Flask(__name__)
 @app.route('/ping')
 def hello_world():
     return 'Pong:)'
+
+
+@app.route('/', methods=['GET'])
+def upload_file():
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
--- run_server.sh
+++ run_server.sh
@@ -5,5 +5,12 @@ HOST='0.0.0.0'
 date
 
 export FLASK_APP=main.py
+export FLASK_ENV=development
+
+if [[ $1 == prod ]]; then
+  export FLASK_ENV=production
+else
+  export FLASK_ENV=development
+fi
 
 flask run --host=${HOST} --port=${PORT}
